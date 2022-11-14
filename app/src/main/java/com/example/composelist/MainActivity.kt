c0045c2bc16a38037c7cfa18f12746b7f51f83cb
KabeerSohail <sohail.mohammad@wenable.com>
2022-11-14 23:31:15 +0530

    Lazy column with dynamic Title, Message and Icon

--- app/src/main/java/com/example/composelist/MainActivity.kt
+++ app/src/main/java/com/example/composelist/MainActivity.kt
@@ -1,185 +1,94 @@
 package com.example.composelist
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composelist.ui.theme.ComposeListTheme
+
+data class BroadcastMessage(
+    val title: String,
+    val message: String,
+    val color: Color
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeListTheme {
-                MyApp(modifier = Modifier.fillMaxSize())
+                BroadcastMessages()
             }
         }
     }
 }
 
 @Composable
-private fun MyApp(modifier: Modifier = Modifier) {
-
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-
-    Surface(modifier, color = MaterialTheme.colors.background) {
-        if(shouldShowOnboarding) {
-            OnBoardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-        } else {
-            Greetings()
-        }
-    }
-}
-
-@Composable
-fun OnBoardingScreen(
-    onContinueClicked: () -> Unit,
-    modifier: Modifier = Modifier
+private fun BroadcastMessages(
+    messages: List<BroadcastMessage> = listOf(
+        BroadcastMessage("sajashjsdj dsjhgdsjh bsjdasjhas usagdjyasg sdgahs ", "hjsdgjhsdgh hjdgfjhdsgfjhsdg fjhgfjdhgfjdyfgjydgfjyg fjysdgfjygdfjegfjhewgfhjewgfjhgewhf wegjhfgwehjfgewhjfghjwefgwjehfg ewjhfgejwhfgwjefgjwehfghjwefgew fgjweh", Color.Gray),
+        BroadcastMessage("Surprise Test Alert", "ashashdasvshj gshjd sdhgsdjgjdewgdedfgej dfjegfjehwg fjewgfjewfg ejwgf", Color.Gray),
+        BroadcastMessage("Fire Alert", "sjfsdhfgdjhgfdjh fdhgfhjdgfhd fhdgfhsdgfhdsgfjdhsfgjshdgfhjdsgfjhsdgfhjsdgfjhdsgfjhdgfjh gfhdjhsfgj ds", Color.Gray),
+        BroadcastMessage("Earthquake Alert", "hjfdhjfdhjfgdhj dgfhdgfdhjf hdgfhdgf hdsgdhjgf sdhfgdhfgdsh fhdsfhsdfghds fdgsfhgsd fhsdjhfgdsjh ", Color.Gray),
+        BroadcastMessage("Meteorite Alert", "sjhsjhdgfhjd fdhjgfjhdsgf dgfjdshgfjsdgfyjesgfjhgjygf udfgeufg sjefg s", Color.Gray),
+        BroadcastMessage("Tsunami Alert", "hdshfdehj fhesgfhje fhegfjhewgfjhew fjhewgjfhegwfewjyf gegjwfgewfgeywfghewdgshdgfhsdgfh dsfghdsgfhdsgfhgdsgfh fgefgsdhfgsegfjyesgjhdgfjhsdgfjhsdgfhsdgfhsdgfewygfhds", Color.Gray),
+        BroadcastMessage("Water pollution Alert", "asfwdsfa fesfwef wewfe wwefew erfergregre gregrr g reg regreger greger gergreg ergerg erg reg regerg erg ergr egerg erg erger gregreg reg erg ergerg erger gergerg erger gerg erg erg reg ergreg reg regregregr hrehr th rth rthth rth trhth th trhrthrer rth trj trjtjyj ", Color.Gray),
+        BroadcastMessage("Air pollution Alert", "ashjsdh efguwe fuew gjuewguje gufegufgewuf ewfgey gfeuyw gfyewgfywe yweuywge ygfewuyfgeuyfgeruy fgewruyfgwuyergfywef gwueygfuwyegfu", Color.Gray),
+        BroadcastMessage("Spaceship crash Alert", "hddfhdhjfg djgfjdgfjegfye fyewgfyewg uf gewuyf fguwyegfueywgfuyewgfuyewg fuywegfuyewgfuygewuywefuyfweuyfeuywf uwey", Color.Gray)
+    )
 ) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Welcome to basics code labs")
-        Button(
-            modifier = Modifier.padding(vertical = 24.dp),
-            onClick = onContinueClicked
-        ) {
-            Text(text = "Continue")
-        }
+    LazyColumn {
+        items(items = messages) { message ->
+            SingleBroadcastMessage(broadcastMessage = message)
+            Divider(color = Color.Black)
+        }  
     }
 }
 
 @Composable
-private fun Greeting(name: String) {
+private fun SingleBroadcastMessage(broadcastMessage: BroadcastMessage) {
 
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colors.primary
-        ),
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    Surface(
+        color = broadcastMessage.color,
     ) {
-        CardContent(name)
-    }
-}
-
-@Composable
-private fun CardContent(name: String) {
-    val expanded = rememberSaveable { mutableStateOf(false) }
-
-    
-    Row(modifier = Modifier
-        .padding(12.dp)
-        .animateContentSize(
-            animationSpec = spring(
-                dampingRatio = Spring.DampingRatioMediumBouncy,
-                stiffness = Spring.StiffnessLow
-            )
-        )) {
         Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(12.dp)
+            modifier = Modifier.padding(horizontal = 12.dp)
         ) {
-            Text(text = "Hello")
-            Text(text = name, style = MaterialTheme.typography.h4.copy(
-                fontWeight = FontWeight.ExtraBold
-            ))
-            
-            if(expanded.value) {
-                Text(text = ("Composem ipsum color sit lazy, " +
-                        "padding theme elit, sed do bouncy. ").repeat(4))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(vertical = 12.dp)
+            ) {
+                Text(
+                    text = broadcastMessage.title,
+                    modifier = Modifier.weight(1f) ,
+                    style = MaterialTheme.typography.h4,
+                    maxLines = 1
+                )
+                Image(painter = painterResource(id = R.drawable.ic_baseline_add_circle_24), modifier = Modifier.size(45.dp) , contentDescription = "Broadcast message icon")
             }
-            
-        }
-        IconButton(onClick = { expanded.value = !expanded.value }) {
-            Icon(imageVector = if(expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore, contentDescription = if(expanded.value) {
-                stringResource(id = R.string.show_less)
-            } else {
-                stringResource(id = R.string.show_more)
-            })
+            Text(text = broadcastMessage.message, modifier = Modifier.padding(bottom = 14.dp))
         }
     }
 }
 
+@Preview(showBackground = true, name = "Single broadcast message", widthDp = 320, heightDp = 320)
 @Composable
-private fun Greetings(
-    modifier: Modifier = Modifier,
-    names: List<String> = List(1000) { "$it" }
-) {
-    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-        items (items = names) { name ->
-            Greeting(name = name)
-        }
-    }
-}
-
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "Dark"
-)
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-private fun GreetingsPreview() {
+fun SingleBroadcastMessagePreview() {
     ComposeListTheme {
-        Greetings()
-    }
-}
-
-@Preview
-@Composable
-fun MyAppPreview() {
-    ComposeListTheme {
-        MyApp(Modifier.fillMaxSize())
-    }
-}
-
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "Dark"
-)
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun DefaultPreview() {
-    ComposeListTheme {
-        MyApp()
-    }
-}
-
-
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnBoardingPreview() {
-    ComposeListTheme {
-        OnBoardingScreen(onContinueClicked = {}) // Do nothing on click.
+        BroadcastMessages()
     }
 }
