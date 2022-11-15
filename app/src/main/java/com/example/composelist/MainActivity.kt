ab21d624e0ae68f3f36f2d44cc675fc2278d9bdf
KabeerSohail <sohail.mohammad@wenable.com>
2022-11-15 17:48:20 +0530

    in progress

--- app/src/main/java/com/example/composelist/MainActivity.kt
+++ app/src/main/java/com/example/composelist/MainActivity.kt
@@ -6,29 +6,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.composelist.ui.theme.ComposeListTheme
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composelist.ui.theme.ComposeListTheme
+import com.example.composelist.ui.theme.LightBlue
+
+data class BroadcastMessage(
+    val title: String,
+    val message: String
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,7 +82,7 @@ fun OnBoardingScreen(
 }
 
 @Composable
-private fun Greeting(name: String) {
+private fun Greeting(broadcastMessage: BroadcastMessage) {
 
     Card(
         colors = CardDefaults.cardColors(
@@ -84,16 +90,17 @@ private fun Greeting(name: String) {
         ),
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        CardContent(name)
+        CardContent(broadcastMessage)
     }
 }
 
 @Composable
-private fun CardContent(name: String) {
+private fun CardContent(broadcastMessage: BroadcastMessage) {
     val expanded = rememberSaveable { mutableStateOf(false) }
 
-    
+
     Row(modifier = Modifier
+        .background(color = LightBlue)
         .padding(12.dp)
         .animateContentSize(
             animationSpec = spring(
@@ -106,16 +113,16 @@ private fun CardContent(name: String) {
                 .weight(1f)
                 .padding(12.dp)
         ) {
-            Text(text = "Hello")
-            Text(text = name, style = MaterialTheme.typography.h4.copy(
-                fontWeight = FontWeight.ExtraBold
-            ))
-            
-            if(expanded.value) {
-                Text(text = ("Composem ipsum color sit lazy, " +
-                        "padding theme elit, sed do bouncy. ").repeat(4))
-            }
-            
+            Text(
+                text = broadcastMessage.title,
+                style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+            )
+
+            Text(
+                text = broadcastMessage.message,
+                maxLines = if(expanded.value) Int.MAX_VALUE else 2
+            )
+
         }
         IconButton(onClick = { expanded.value = !expanded.value }) {
             Icon(imageVector = if(expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore, contentDescription = if(expanded.value) {
@@ -130,11 +137,19 @@ private fun CardContent(name: String) {
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = List(1000) { "$it" }
+    broadcastMessages: List<BroadcastMessage> = listOf(
+        BroadcastMessage("Exam alert", "Please click on following link to start your exam -> www.netflix.com. For any queries please refer to the following link www.google.com"),
+        BroadcastMessage("Cyclone alert", "Please leave the town ASAP"),
+        BroadcastMessage("Tsunami alert", "Please don't go to beach"),
+        BroadcastMessage("Earth quake alert", "Please get out of the build"),
+        BroadcastMessage("Fire alert", "Please get out of the building"),
+        BroadcastMessage("What if title is very long", "Please get out of the building"),
+        BroadcastMessage("What if title is very very very very very long", "Please get out of the building"),
+    )
 ) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-        items (items = names) { name ->
-            Greeting(name = name)
+        items (items = broadcastMessages) { broadcastMessage ->
+            Greeting(broadcastMessage = broadcastMessage)
         }
     }
 }
