e08950133a9fd848eea179d3f6a32d808f3787d3
KabeerSohail <sohail.mohammad@wenable.com>
2022-11-16 23:19:04 +0530

    done

--- app/src/main/java/com/example/composelist/MainActivity.kt
+++ app/src/main/java/com/example/composelist/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
@@ -28,6 +29,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -133,13 +135,12 @@ private fun CardContent(broadcastMessage: BroadcastMessage) {
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
 
-                Surface(modifier = Modifier.weight(1F, false)) {
+                Surface(modifier = Modifier.weight(1F, false).clip(RoundedCornerShape(10.dp))) {
                     AndroidView(factory = { titleView }) { broadcastTitle ->
-                    broadcastTitle.background = mContext.getDrawable(R.drawable.lite_gray_rectangle)
                         broadcastTitle.setPadding(24, 0, 24, 0)
                         broadcastTitle.text = broadcastMessage.title
                         broadcastTitle.textSize = 30F
-//                        broadcastTitle.setBackgroundColor(mContext.resources.getColor(R.color.gray_100))
+                        broadcastTitle.setBackgroundColor(mContext.resources.getColor(R.color.text_background))
                         broadcastTitle.ellipsize = TextUtils.TruncateAt.MARQUEE
                         broadcastTitle.isSingleLine = true
                         broadcastTitle.marqueeRepeatLimit = -1
